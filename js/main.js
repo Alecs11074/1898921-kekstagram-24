f2a553c49813468497e48c907afd13813857edc6
John Deshchenko <denisenko.iwan2017@yandex.ru>
2021-10-27 21:41:08 +0300

    Отрисуй меня полностью (часть 2)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 import {createMassive} from './data.js';
 import {createPicture} from './create-picture.js';
+import { bigPictureData } from './create-big-picture.js';
 createMassive();
 createPicture();
+bigPictureData();
